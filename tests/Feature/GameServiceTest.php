88501479840edfb029cebbf50d2a6bc3c9a90b93
christian.celora@gmail.com <christian.celora@gmail.com>
2021-01-15 22:26:36 +0100

    added test GameService

---
+++ tests/Feature/GameServiceTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\ColorGuessr\GameGenerator\EasyGame;
+use App\Services\GameService;
+
+class GameServiceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    
+    /** @var int $test_game_id game id for test */
+    private $test_game_id;
+    /** @var EasyGame $game_service */
+    private $game_service;
+
+    public function setUp(){
+        // Create test game
+        $game_generator = new EasyGame();
+        $this->test_game_id = $game_generator->createGame("fake_session_id");
+        $this->game_service = GameService::make($this->test_game_id);
+    }
+
+    public function testGetGameData(){
+        $actual_data = $this->game_service->getGameData();
+        $this->assertEquals($this->test_game_id, $actual_data["game_id"]);
+        $this->markTestIncomplete("test incomplete");
+    }
+
+    public function testGetResultsGameData(){
+        $this->markTestIncomplete("test incomplete");
+    }
+
+    public function testUpdateUserGuess(){
+        $this->markTestIncomplete("test incomplete");
+    }
+
+    public function testGetSolutionData(){
+        $this->markTestIncomplete("test incomplete");
+    }
+
+    public function testCalcScore(){
+        $this->markTestIncomplete("test incomplete");
+    }
+
+    public function testGetCurrentStepModel(){
+        $this->markTestIncomplete("test incomplete");
+    }
+}
