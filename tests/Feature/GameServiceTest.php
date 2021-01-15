df8c3de95b6ce2c3c06e6f7e9c9909fa120d363a
christian.celora@gmail.com <christian.celora@gmail.com>
2021-01-15 22:30:40 +0100

    added test GameService

--- tests/Feature/GameServiceTest.php
+++ tests/Feature/GameServiceTest.php
@@ -7,28 +7,29 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\ColorGuessr\GameGenerator\EasyGame;
 use App\Services\GameService;
+use App\Services\DTO\GameDto;
 
 class GameServiceTest extends TestCase
 {
     use RefreshDatabase;
 
-    
     /** @var int $test_game_id game id for test */
     private $test_game_id;
     /** @var EasyGame $game_service */
     private $game_service;
 
-    public function setUp(){
+    public function setUp(): void{
+        parent::setUp();
+        $this->seed(\Database\Seeders\ColorTableSeeder::class);
         // Create test game
         $game_generator = new EasyGame();
         $this->test_game_id = $game_generator->createGame("fake_session_id");
-        $this->game_service = GameService::make($this->test_game_id);
+        $this->game_service = GameService::make(new GameDto($this->test_game_id));
     }
 
     public function testGetGameData(){
         $actual_data = $this->game_service->getGameData();
         $this->assertEquals($this->test_game_id, $actual_data["game_id"]);
-        $this->markTestIncomplete("test incomplete");
     }
 
     public function testGetResultsGameData(){
